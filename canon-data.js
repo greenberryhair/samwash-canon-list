76b04bff78cb5224270176391cf8624f8c1fc3a3
greenberryhair <hair.bonner@live.com>
2026-09-05 21:36:04 -0500

    Update canon-data.js

--- canon-data.js
+++ canon-data.js
@@ -263,7 +263,7 @@ window.CanonDirectoryData = {
               "name": "Betsey Astor Thomas",
               "info": "PB:  Emmy Rossum Age: 44",
               "affiliations": [
-                "DECEASED"
+                "DECEASED_LEGACY"
               ],
               "footer": "CAUSE OF DEATH: Murdered by Thames and Mercedes Vanderbilt",
               "deceasedLabel": "DECEASED",
